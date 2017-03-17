6a30a933bbcc51cfed14d7775d7df93646b70298
Ralle1968 <graef.ralf@gmail.com>
2017-03-17 22:14:48 +0100

    Basic API-read

--- WeatherAPI/src/de/weatherapi/MainWeatherAPI.java
+++ WeatherAPI/src/de/weatherapi/MainWeatherAPI.java
@@ -8,10 +8,10 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class Main {
+public class MainWeatherAPI {
 
 	public static void main(String[] args) throws Exception {
-		String uri = "http://api.openweathermap.org/data/2.5/forecast?q=Bonn&mode=xml&appid=381a845f689c1bcd27937961e5af5b67";
+		String uri = "http://api.openweathermap.org/data/2.5/forecast?q=Berlin&mode=xml&appid=381a845f689c1bcd27937961e5af5b67";
 		
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
@@ -22,7 +22,17 @@ public class Main {
 		for (int x = 0; x < times.getLength(); x++) {
 			Node time = times.item(x);
 			NamedNodeMap timeAttributes = time.getAttributes();
-			System.out.println(timeAttributes.getNamedItem("from"));
+			String timestamp = timeAttributes.getNamedItem("from").getNodeValue();
+			
+			NodeList children = time.getChildNodes();
+			for (int y = 0; y < children.getLength(); y++) {
+				Node child = children.item(y);
+				if (child.getNodeName() == "temperature") {
+					String temperature = child.getAttributes().getNamedItem("value").getNodeValue()+273;
+					System.out.println(timestamp + ": " + temperature);
+				}
+			}
+			
 		}
 		
 	
