f6ece8a066165cc333a613686d22dc43033281da
Ralle1968 <graef.ralf@gmail.com>
2017-03-17 15:08:36 +0100

    Init

---
+++ WeatherAPI/src/de/weatherapi/Main.java
@@ -0,0 +1,9 @@
+package de.weatherapi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String uri = "http://api.openweathermap.org/data/2.5/forecast?q=Bonn&mode=xml&appid=381a845f689c1bcd27937961e5af5b67";
+	}
+
+}
